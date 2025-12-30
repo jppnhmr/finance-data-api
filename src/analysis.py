640afc3ca2bc569a27aab6e820489b0251b094cc
jppnhmr <jppnhmr@gmail.com>
2025-12-30 14:12:52 +0000

    Updated notebook and analysis
    Updated analysis with data gathering functions for policy interest rate and index metrics. Added data to lines formatting function. Added plotting function for interest rates against indices. Updated notebook to use new analysis functions and plot new graph.

--- src/analysis.py
+++ src/analysis.py
@@ -10,7 +10,42 @@ from typing import List
 
 import src.database as db
 
-# Make a graph of List[(x_values, y_values)]
+# Data Gathering #
+def get_pir_datas():
+    metric = 'policy interest rate'
+    pir_datas = []
+
+    pir_datas.append(db.get_data_country_metric(
+        db.get_country_id('US'),
+        db.get_metric_id(metric)))
+    pir_datas.append(db.get_data_country_metric(
+        db.get_country_id('UK'),
+        db.get_metric_id(metric)))
+    
+    return pir_datas
+
+def get_idx_datas():
+    idx_datas = []
+
+    idx_datas.append(db.get_data_global_metric(db.get_metric_id('global food index')))
+    idx_datas.append(db.get_data_global_metric(db.get_metric_id('global energy index')))
+    idx_datas.append(db.get_data_global_metric(db.get_metric_id('global all commodities index')))
+
+    return idx_datas
+
+# Data Handling #
+def data_to_lines(datas: List[List[tuple]]):
+    lines = []
+    for data in datas:
+        line = []
+        for row in data:
+            date = datetime.strptime(row[0],'%Y-%m-%d')
+            value = row[1]
+            line.append((date, value))
+        lines.append(line)
+    return lines
+
+# Plotting #
 def plot_data(data: List[tuple], title, y_axis, y_unit):
     dates = []
     values = []
@@ -78,6 +113,33 @@ def plot_multi_data(datas: List[List[tuple]], title, y_axis, y_unit):
     plt.xlabel('date')
 
     plt.title(title)
+    plt.show()
+
+def plot_interest_against_index(pir_datas: List[List[tuple]], 
+                                idx_datas: List[List[tuple]]):
+    
+    pir_lines = data_to_lines(pir_datas)
+    idx_lines = data_to_lines(idx_datas)
+
+    fig, ax1 = plt.subplots(figsize=(10,6))
+    for line in pir_lines:
+        dates = [point[0] for point in line]
+        values = [point[1] for point in line]
+        ax1.plot(dates, values, label='interest rate', color='blue')
+    ax1.set_xlabel('Year')
+    ax1.set_ylabel('Interest Rate (%)', color='tab:blue')
+    
+    ax2 = ax1.twinx()
+    for line in idx_lines:
+        dates = [point[0] for point in line]
+        values = [point[1] for point in line]
+        ax2.plot(dates, values, label='index', color='red')
+    ax2.set_xlabel('Year')
+    ax2.set_ylabel('Index Values', color='tab:red')
+
+    plt.title('Interest Rates vs Indices')
+    plt.grid(True, alpha=0.3)
+
     plt.show()
 
 def cli_plot_data():
